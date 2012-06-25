49ffbc67a3fc47933e12e6e34b3eb93804cbe18c
Sean Escriva <sean.escriva@gmail.com>
2012-06-25 13:23:49 -0700

    remove cookbooks not managed by knife berks

--- cookbooks/gdash/recipes/builtins.rb
+++
@@ -1,8 +0,0 @@
-
-include_recipe 'gdash::scrub_builtins'
-
-if(node[:gdash][:builtins])
-  builtin_dashboards do
-    dashboards node[:gdash][:builtins]
-  end
-end
--- cookbooks/gdash/recipes/scrub_builtins.rb
+++
@@ -1,5 +0,0 @@
-# This recipe is used to scrub out currently saved
-# dashboards allowing repopulation with correct/
-# expected final contents
-
-node.set[:gdash][:builtin_dashboards] = {}
